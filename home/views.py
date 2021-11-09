c92d35daf611a10ec1af22e3d803836b85f5006c
xvimnt <jvmonteros98@gmail.com>
2021-11-09 17:25:46 -0600

    Added style

--- home/views.py
+++ home/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeView(TemplateView):
-    template_name = 'home/welcome.html'
-    extra_content = {'today': datetime.today()}
+    template_name = 'home/editor.html'
+    extra_context = {'today': datetime.today()}
 
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'home/authorized.html'
